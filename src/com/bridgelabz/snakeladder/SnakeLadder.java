964b8ed53507020d41e8a869ba4ea09e1787ba7b
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-10 13:12:54 +0530

    Welcome to Snake-Ladder game git add .!

---
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake-ladder game !!! ");
+    }
+}
